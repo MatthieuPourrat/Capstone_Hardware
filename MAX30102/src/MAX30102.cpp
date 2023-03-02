b312c1b3bc67ce08169cc2caf73f653e809a5781
Matthieu Pourrat <matthieupourrat@Matthieus-MacBook-Air.local>
2023-03-01 22:10:42 -0500

    MAX30102

--- MAX30102/src/MAX30102.cpp
+++ MAX30102/src/MAX30102.cpp
@@ -5,7 +5,9 @@
 
 MAX30102::MAX30102()
 {
-
+    ir = 0;
+    red = 0;
+    hr = 0;
 }
 
 void MAX30102::writeRegister(uint8_t reg, uint8_t data)
@@ -28,16 +30,18 @@ uint8_t MAX30102::readRegister(uint8_t reg)
         return -1;
 }
 
-uint8_t MAX30102::readRegisterFIFO()
+void MAX30102::readRegisterFIFO()
 {
+    int counter = 0;
     Wire.beginTransmission(SLAVE_ADDR);
     Wire.write(FIFO_DATA);
     Wire.endTransmission(false);
     Wire.requestFrom(SLAVE_ADDR, 6); 
-    if(Wire.available())
-        return Wire.read();
-    else
-        return -1;
+    while(Wire.available())
+    {   
+        answers[counter] = Wire.read();
+        counter++;
+    }
 }
 
 void MAX30102::enable_spo2()
@@ -53,4 +57,41 @@ void MAX30102::clearFIFO()
     this->writeRegister(0x04,0x00);
     this->writeRegister(0x05,0x00);
     this->writeRegister(0x06,0x00);
+}
+
+uint32_t MAX30102::getIR()
+{
+    ir = answers[0];
+    for(int i = 1; i < 3; i++)
+    {
+        ir <<= 8;
+        ir |= answers[i];
+    }
+    ir &= 0x3FFFF;
+    Serial.println(ir);
+    return ir;
+}
+
+uint32_t MAX30102::getRed()
+{
+    red = answers[3];
+    for(int i = 4; i < 6; i++)
+    {
+        red <<= 8;
+        red |= answers[i];
+    }
+    red &= 0x3FFFF;
+    Serial.println(red);
+    return red;
+}
+
+float MAX30102::computeHR()
+{
+    this->readRegisterFIFO();
+    this->getRed();
+    this->getIR();
+    if (ir < 2000 && red < 2000)
+        return -1;
+    else
+        return hr = ((float)red/(float)ir)*63;
 }
