7684e62b19df721d66f4e86d21c501a8d7efe12b
MatthieuPourrat <50846358+MatthieuPourrat@users.noreply.github.com>
2023-03-01 19:05:54 -0500

    Small Changes

--- MAX30102/src/main.cpp
+++ MAX30102/src/main.cpp
@@ -1,13 +1,19 @@
 #include <Arduino.h>
 #include <Wire.h>
+#include "MAX30102.h"
 
-
+MAX30102 device_max30102;
+uint8_t answer =  0;
 
 void setup() {
   Serial.begin(9600);
   Wire.begin();
+  device_max30102.clearFIFO();
+  device_max30102.enable_spo2();
 }
 
 void loop() {
-  
+  Serial.println(device_max30102.readRegisterFIFO());
+  Serial.println();
+  delay(1000);
 }
